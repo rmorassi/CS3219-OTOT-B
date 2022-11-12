28351fd6ecfc940c8c17d02f6a80f9f2eb0cf161
Roberto <s2026885@ed.ac.uk>
2022-11-12 16:59:26 +0800

    added frontend to serverless function

---
+++ frontend/src/ServerlessInteraction.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+var link = "http://localhost:8080";
+var apiLink = "/api/tempAvg";
+
+class ServerlessInteraction extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {dataPoints: null, avg: null};
+
+        this.processRequest = this.processRequest.bind(this);
+    }
+
+    processRequest(e) {
+        var date = this._date.value + ":00"
+
+        console.log(date);
+
+        fetch(link + apiLink + "?date_time=" + date, {
+            method: "GET"
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            this.setState({dataPoints: JSON.stringify(data.dataPoints), avg: data.avg.toFixed(2)});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+        e.preventDefault(); // Prevent page reload
+    }
+
+    render() {
+        var {dataPoints, avg} = this.state;
+
+        dataPoints = (dataPoints) ? dataPoints : "";
+        avg = (avg) ? avg + "°C": "";
+
+        return (
+            <div className="serverlessInteractionMain">
+                <div className="header">
+                    <form onSubmit={this.processRequest}>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">Input date and time</span>
+                            {/* <input class="form-control" ref={(a) => this._gender = a} placeholder="Male"></input> */}
+                            <input type="datetime-local" ref={(a) => this._date = a} ></input>
+                        </div>
+                        <button class="btn btn-primary" type="submit">Get average temperature</button>
+                        <br></br>
+                        <br></br>
+                    </form>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">Average temperature recorded: {avg}</li>
+                    <li class="list-group-item">Number of weather stations recorded at that time: {dataPoints}</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default ServerlessInteraction;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import ApiInteraction from "./ApiInteraction";
+import ServerlessInteraction from "./ServerlessInteraction";
 
 var destination = document.querySelector("#container")
 
 ReactDOM.render(
     <div>
         <ApiInteraction />
+        <hr/>
+        <ServerlessInteraction />
     </div>,
     destination
 );
